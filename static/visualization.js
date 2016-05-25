8b6ed0d74d2dbcae9ff6afe191c4b035ed6a2920
exp0nge <mdislamwork@gmail.com>
2016-05-25 17:47:58 -0400

    Added labels for graphs

---
+++ static/visualization.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+    d3.json('data.json', function (data) {
+        var genderPieChart = dc.pieChart('#genderPie');
+        var marriagePieChart = dc.pieChart('#marriagePie');
+        var ageDefaultersChart = dc.barChart('#ageDefaulters');
+        var limitBalDefaultersChart = dc.lineChart('#limitBalDefaulters');
+        var ndx = crossfilter(data);
+        var all = ndx.groupAll();
+        var genderDim = ndx.dimension(function (d) {
+            var sex = d['SEX'];
+            if (sex === 1) {
+                return 'Male';
+            } else if (sex == 2) {
+                return 'Female';
+            } else {
+                return 'Unknown';
+            }
+        });
+        var marriageDim = ndx.dimension(function (d) {
+            var status = d['MARRIAGE'];
+            if (status === 1) {
+                return 'Married';
+            } else if (status == 2) {
+                return 'Single';
+            } else if (status === 3) {
+                return 'Others';
+            } else {
+                return 'Unknown';
+            }
+        });
+
+        genderPieChart
+            .width(180)
+            .height(180)
+            .radius(80)
+            .dimension(genderDim)
+            .group(genderDim.group());
+        genderPieChart.controlsUseVisibility(true);
+        $('#genderPie > a').on('click', function (e) {
+            genderPieChart.filterAll();
+            dc.redrawAll();
+            e.preventDefault();
+        });
+
+        marriagePieChart
+            .width(180)
+            .height(180)
+            .radius(80)
+            .dimension(marriageDim)
+            .group(marriageDim.group());
+        marriagePieChart.controlsUseVisibility(true);
+        $('#marriagePie > a').on('click', function (e) {
+            marriagePieChart.filterAll();
+            dc.redrawAll();
+            e.preventDefault();
+        });
+        ageMinMax = d3.extent(data, function (d) {
+            return d['AGE'];
+        });
+        ageDefaultersDim = ndx.dimension(function (d) {
+            return d['AGE'];
+        });
+
+        ageDefaultersChart
+            .width(900)
+            .height(250)
+            .dimension(ageDefaultersDim)
+            .group(ageDefaultersDim.group().reduceSum(dc.pluck('default payment next month')))
+            .x(d3.scale.linear().domain(ageMinMax))
+            .xAxisLabel('Age')
+            .yAxisLabel('People Who Defaulted');
+
+        limitDefaultersDim = ndx.dimension(function (d) {
+            return d['LIMIT_BAL'];
+        });
+
+        limitBalMinMax = d3.extent(data, function (d) {
+            return d['LIMIT_BAL'];
+        });
+
+        limitBalDefaultersChart
+            .width(1000)
+            .height(200)
+            .dimension(limitDefaultersDim)
+            .group(limitDefaultersDim.group().reduceSum(dc.pluck('default payment next month')))
+            .x(d3.scale.linear().domain(limitBalMinMax))
+            .renderArea(true)
+            .mouseZoomable(true)
+            .xAxisLabel('Amount of Credit Given')
+            .yAxisLabel('People');
+
+        dc.renderAll();
+
+    });
+});
